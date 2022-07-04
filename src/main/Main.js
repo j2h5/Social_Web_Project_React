e536a92ed008edbe7caf5edd25b6d066651aec59
YOONSEOK1997 <yoonsuk0629@gmail.com>
2022-07-04 19:55:21 +0900

    main, route main 수정

--- src/main/Main.js
+++ src/main/Main.js
@@ -6,6 +6,8 @@ import MainChallenge from './Mainchallenge';
 import MainMoim from './Mainmoim';
 import Mainclass from './Mainclass'; 
 import Footer from './Footer';
+import './index.css'
+
 const Main = () => {
     return (
         <div>
