9714b1ab1866d180d9a33d7530bba0fffbfed553
YOONSEOK1997 <yoonsuk0629@gmail.com>
2022-07-06 01:14:22 +0900

    새벽코딩

--- src/main/Demo.js
+++ src/main/Demo.js
@@ -3,6 +3,7 @@ import './Demo.css'
 import challengeImg from './images/challengeImg.png'
 import moimImg from './images/moimImg.png'
 import classImg from './images/classImg.png'
+import { NavLink } from 'react-router-dom'
 const Demo = () => {
     return (
         <div className='demo' id='demo'>
@@ -10,21 +11,26 @@ const Demo = () => {
             <div class="gallerylist">
 	<ul>
 		<li>
-			<a href="">
+        <NavLink to="/challenge/list">
+			<a>
+            
 				<div class="screen">
 					<div class="top">Challenge</div>
 					<div class="bottom">챌린지</div>
 					<img src={challengeImg}/>
 				</div>
 				<div>
-					<h3>Challenge with other people</h3>
+					<h3>Challenge with your mate</h3>
 				</div>
 			</a>
+        </NavLink>
 		</li>
 		<li>
-			<a href="">
+            <NavLink to="/moim/list">
+			<a>
 				<div class="screen">
 					<div class="top">METTING</div>
+                
 					<div class="bottom">모임</div>
 					<img src={moimImg}/>
 				</div>
@@ -32,18 +38,22 @@ const Demo = () => {
 					<h3>Group with your buddy</h3>
 				</div>
 			</a>
+            </NavLink>
 		</li>
 		<li>
-			<a href="">
+            <NavLink to="/class/list">
+			<a>
 				<div class="screen">
 					<div class="top">CLASS</div>
 					<div class="bottom">클래스</div>
+                
 					<img src={classImg}/>
 				</div>
 				<div>
-					<h3>Study with your mate</h3>
+					<h3>Study with other people</h3>
 				</div>
 			</a>
+            </NavLink>
 		</li>
 	</ul>
 </div>
---
+++ src/moim/MoimDetailForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './MoimDetailForm.css'
+const MoimDetailForm = () => {
+    return (
+        <div>
+            <div className='moimdetail_content'>
+                <div className='moimdetail_top'> 
+               <div className='moimdetail_photo'>사진 </div>
+               <div className='moimdetail_title'>제목</div>
+               <div className='moimdetail_place'>장소</div>
+               <div className='moimdetail_date'>날짜</div>
+                <div className='moimdetail_people'>현재인원/모집정원</div>
+                
+
+</div>
+        </div>
+        </div>
+    );
+};
+
+export default MoimDetailForm;
