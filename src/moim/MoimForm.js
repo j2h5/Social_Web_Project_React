faee08921d71cad2703a46efd12db1ad3c5a9d90
Jungha <kidultree@naver.com>
2022-07-11 12:02:36 +0900

    [수정] moim form css 수정

--- src/moim/MoimForm.js
+++ src/moim/MoimForm.js
@@ -1,82 +1,91 @@
-import React from 'react';
-import '../cssFolder/Challenge.css';
-
+import React, {useState, useEffect} from "react";
+import Editor from '../class/EditorWithUseQuill';
+import '../cssFolder/MoimForm.css';
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 const MoimForm = () => {
-    return(
-        <div>
-        <h1>모임등록</h1>
-            <div className="Meeting">
-                {/* memo form */}
-                <table style={{width:'800px', margin:'auto'}}>
-                <thead>
-                    <tr>
-                        <td className="td_1">모임명</td>
-                        
-                        <td colSpan={3}  style={{width:'720px'}}>
-                        <input type='text' className="form-control"
-                        style={{width:'600px'}} placeholder="모임명 입력"
+    const [data,setData] = useState({
+        moim_name:'',
+        moim_leader:'',
+        moim_place:'',
+        moim_count:'',
+        moim_photo:'',
+        moim_content:''
+    });
+    const navi = useNavigate();
+    //submit 시 호출될 함수
+    const onSave = (e) =>{
+        e.preventDefault(); //기본 이벤트를 무효화
+            const url = "http://localhost:9009/moim/insert";
+            axios.post(url, data)
+            .then(res=>{
+                alert("insert 성공")
+                //navi("/login")
+            });
+    }
+    //data 관련 데이터 입력시 호출
+    const onDataChange =(e) =>{
+        const {name,value}=e.target;
+        setData({
+            ...data,
+            [name]:value
+        })
+        console.log(data);
+    }
+    return (
+        <div style={{marginLeft:"500px"}}>
+            <form className="form-inline" onSubmit={onSave}>
+            <div></div>
+            <br></br><br></br><br></br>
+            <h1> 모임등록</h1>
+            <br></br>
+             <div className="label1" >모임명</div>
+             <input type='text' className="form-control" name="moim_name"
+                        style={{width:'650px', height:'70px'}} placeholder="모임명 입력" onChange={onDataChange}
                         />
-                        </td>
-                    </tr>
-                </thead>
-                <tbody>
-                    
-                    <tr>
-                        <td  className="td_1">방장</td>
-                        <td  className="td_2">
-                        <input type='text' className="form-control"
-                        style={{width:'210px', height:'30px'}} placeholder="방장"
-                  
+             <br></br>
+             <div style={{display:'block'}}>
+                <div style={{display:"inline-block"}}>
+             <div className="label1">방장</div>
+             <input type='text' className="form-control" name="moim_leader" onChange={onDataChange}
+                        style={{width:'200px', height:'70px',display:'inline-block'}} placeholder="방장"
                         />
-                        </td>
-                        
-                        <td  className="td_1" >장소</td>
-                        <td className="td_2">
-                        <input type='text' className="form-control"
-                        style={{width:'210px', height:'30px'}} placeholder="장소"
-                      
+                        </div>
+                        <div style={{display:"inline-block"}}>
+             <div className="label1">장소</div>
+             <input type='text' className="form-control" name="moim_place" onChange={onDataChange}
+                        style={{width:'200px', height:'70px',display:'inline-block'}} placeholder="장소"
                         />
-                        </td>
-                        
-                    </tr>
-                  
-                    <tr>
-                        <td  className="td_1">참여인원수</td>
-                        <td>
-                        <input type='text' className="td_2"
-                        style={{width:'210px'}} placeholder="닉네임 입력"
-                  
+                        </div>
+             </div>
+             <br></br>
+             <div style={{display:'block'}}>
+                <div style={{display:"inline-block"}}>
+             <div className="label1">참여인원수</div>
+             <input type='text' className="form-control" name="moim_count" onChange={onDataChange}
+                        style={{width:'200px', height:'70px',display:'inline-block'}} placeholder="참여인원수"
                         />
-                       
-                        </td>
-                            
-                        <td  className="td_1">사진</td>
-                        <td>
-                        <input type='file' className="td_2"
-                        style={{width:'210px', height:'30px'}} placeholder="사진"
-               
+                        </div>
+                        <div style={{display:"inline-block"}}>
+             <div className="label1">사진</div>
+             <input type='file' className="form-control" name="moim_photo" onChange={onDataChange}
+                        style={{width:'200px', height:'70px',display:'inline-block'}} placeholder="사진"
                         />
-                        </td>
-
-                    </tr>
-
-                </tbody>
-                </table>
-                <div>
-                    <div>
-                        <b>모집글 작성</b>
-                    </div>
-                    <div>
-                        <input type='text' className="form-control"
-                        style={{width:'600px', height:'230px'}} placeholder="메세지 입력 후 엔터를 누르세요"
-                        />
-                    </div>
+                        </div>
+             </div>
+            <br></br><br></br>
+            
+                <div style={{ width:"880px"}}>
+                    <h3>모집글 작성</h3>
+                <Editor placeholder={'Write something...'} />
                 </div>
-                <button type="submit">등록</button>
-            </div>
-    </div>
-)
-}
-
-
+                <br/><br/><br/>
+                <button class="w-btn w-btn-gra3 w-btn-gra-anim" type="submit">
+                    모임 등록
+                </button>
+                <br/><br/><br/><br/><br/>
+                </form>
+        </div>
+    );
+};
 export default MoimForm;
